15b9f2af78f0a07435d20b5a056ec29833ce95f4
Chandran,Shunmugavel <shunchandran@paypal.com>
2015-04-19 13:26:28 -0700

    Configuring spring mvc rest project.

---
+++ trendster/src/main/resources/com/trendanora/config/AppConfig.java
@@ -0,0 +1,11 @@
+
+package com.trendanora.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@Configuration 
+@ComponentScan("com.trendanora") 
+@EnableWebMvc   
+public class AppConfig {  
+} 
---
+++ trendster/src/main/resources/com/trendanora/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.trendanora.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/data")
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String getPersonDetail(@RequestParam(value = "id",required = false,
+	                                                    defaultValue = "0") Integer id) {
+		return new String("sample : "+id);
+	}
+
+} 
